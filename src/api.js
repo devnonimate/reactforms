3474475921689e8eda0afc4905616665f7b2c7b1
devnonimate <devprojectnewera@gmail.com>
2024-12-28 09:13:52 -0300

    dois

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://<sua-url-do-replit>";
+const BASE_URL = "https://910a-2a01-4f9-c012-242-00-1.ngrok-free.app";
 
 export const register = async (userData) => {
     const response = await fetch(`${BASE_URL}/register`, {
